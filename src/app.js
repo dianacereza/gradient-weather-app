c7476c26891d3c64835f450fd92b52c788369671
Diana Najera <diananajerarmz@gmail.com>
2022-02-11 17:11:32 -0600

    Set current temperature, description, humidity and wind

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 function showTemperature(response){
+    console.log(response.data);
 let temperatureNumber = document.querySelector("#temperature-number-now");
 temperatureNumber.innerHTML = Math.round(response.data.main.temp);
 
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = response.data.name;
+
+let descriptionStatus = document.querySelector("#temperature-status-now");
+descriptionStatus.innerHTML = response.data.weather[0].description;
+
+let humidityStatus = document.querySelector("#humidity-now");
+humidityStatus.innerHTML = Math.round(response.data.main.humidity);
+
+let windStatus = document.querySelector("#wind-now");
+windStatus.innerHTML =Math.round(response.data.wind.speed);
+
 }
 let apiKey="338ad8d174dc01460bda54dd03bef62b";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
