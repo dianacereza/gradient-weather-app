810201848646b05be22352896405dd89f73c242e
Diana Najera <diananajerarmz@gmail.com>
2022-02-11 17:58:07 -0600

    Updated actual Date and time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10){
+    minutes = `0${minutes}`;
+    }
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response){
-    console.log(response.data);
+
 let temperatureNumber = document.querySelector("#temperature-number-now");
 temperatureNumber.innerHTML = Math.round(response.data.main.temp);
 
@@ -15,9 +30,12 @@ humidityStatus.innerHTML = Math.round(response.data.main.humidity);
 let windStatus = document.querySelector("#wind-now");
 windStatus.innerHTML =Math.round(response.data.wind.speed);
 
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
 }
+
 let apiKey="338ad8d174dc01460bda54dd03bef62b";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
 
 axios.get(apiUrl).then(showTemperature);
